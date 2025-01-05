01d80ca96b408c149fcd5e75cc1d15b7939ed22b
Владислав <vladislav@MacBook-Air-Vladislav.local>
2025-01-05 21:19:53 +0300

    ref: removed system prefered theme logic

--- src/contexts/ThemeContext.tsx
+++ src/contexts/ThemeContext.tsx
@@ -3,20 +3,14 @@ import { FC, ReactNode, createContext, useState } from 'react'
 
 export const ThemeContext = createContext<ThemeContextValues | null>(null)
 
-const getPreferredTheme = () => {
-  if (window.matchMedia('(prefers-color-scheme: dark)').matches) return 'dark'
-  return 'light'
-}
-
 const getTheme = () => {
   const storedTheme = localStorage.getItem('theme')
 
   if (storedTheme === 'light' || storedTheme === 'dark') {
     return storedTheme
-  } else {
-    const preferredTheme = getPreferredTheme()
-    return preferredTheme
   }
+
+  return 'light'
 }
 
 export const ThemeContextProvider: FC<{ children: ReactNode }> = ({
