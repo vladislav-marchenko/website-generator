fcc56734a26e506a2b7219b93c5e29896713c0f1
Владислав <vladislav@MacBook-Air-Vladislav.local>
2025-01-19 00:13:59 +0300

    feat: added logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
 import { HeaderNav } from './HeaderNav'
-import { PROJECT_NAME } from '@/consts'
+import Logo from '@/assets/images/logo.svg'
 import { Link } from 'react-router-dom'
 
 export const Header = () => {
@@ -7,7 +7,7 @@ export const Header = () => {
     <header className='sticky top-0 z-20 w-full border-b border-neutral-200 bg-white p-5 shadow-sm dark:border-neutral-700 dark:bg-neutral-900'>
       <div className='container flex items-center justify-between'>
         <Link to='/' className='text-xl font-bold uppercase'>
-          {PROJECT_NAME}
+          <img src={Logo} alt='Aurora' />
         </Link>
         <HeaderNav />
       </div>
