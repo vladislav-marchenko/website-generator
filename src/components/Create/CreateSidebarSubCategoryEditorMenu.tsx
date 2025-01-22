e3798add19f37110d14aaa8957d81e789f25ae2b
Владислав <vladislav@MacBook-Air-Vladislav.local>
2025-01-23 00:01:56 +0300

    feat: changed editor menu animation duration

--- src/components/Create/CreateSidebarSubCategoryEditorMenu.tsx
+++ src/components/Create/CreateSidebarSubCategoryEditorMenu.tsx
@@ -32,7 +32,7 @@ export const CreateSidebarSubCategoryEditorMenu: FC = () => {
           initial={{ x: '-100%' }}
           animate={{ x: 0 }}
           exit={{ x: '-100%' }}
-          transition={{ duration: 0.3 }}
+          transition={{ duration: 0.2 }}
           className='absolute inset-0 z-10 overflow-y-auto bg-white dark:bg-neutral-900'
         >
           <CreateSidebarHeader onClick={() => setActiveSubCategory(null)} />
