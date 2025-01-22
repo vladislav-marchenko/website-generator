19cae07bd12491857971242cb765d942a2c4cb41
Владислав <vladislav@MacBook-Air-Vladislav.local>
2025-01-22 23:22:52 +0300

    fix: improved editor types safety

--- src/components/Create/CreateSidebarSubCategoryEditorMenu.tsx
+++ src/components/Create/CreateSidebarSubCategoryEditorMenu.tsx
@@ -3,13 +3,27 @@ import { CreateSidebarBack } from './CreateSidebarBack'
 import { editorFields } from '@/consts'
 import { TemplateContext } from '@/contexts/TemplateContext'
 import type { TemplateContextValues } from '@/types/contexts'
+import { TemplateSubCategoryField } from '@/types/templates'
 import { AnimatePresence, motion } from 'motion/react'
 import { FC, useContext } from 'react'
+
+const getFields = (activeSubCategory: TemplateSubCategoryField | null) => {
+  if (!activeSubCategory) return null
+  const type = activeSubCategory.type
+
+  if (editorFields.hasOwnProperty(type)) {
+    return editorFields[type as keyof typeof editorFields]
+  }
+
+  return null
+}
 
 export const CreateSidebarSubCategoryEditorMenu: FC = () => {
   const { activeSubCategory, setActiveSubCategory } = useContext(
     TemplateContext
   ) as TemplateContextValues
+
+  const fields = getFields(activeSubCategory)
 
   return (
     <AnimatePresence>
@@ -24,13 +38,17 @@ export const CreateSidebarSubCategoryEditorMenu: FC = () => {
           <CreateSidebarBack onClick={() => setActiveSubCategory(null)} />
 
           <div className='flex flex-col gap-8 p-4'>
-            {editorFields[activeSubCategory.type]?.map(
-              ({ label, element: Element }) => (
-                <div key={label} className='flex flex-col gap-2'>
-                  <Label>{label}</Label>
-                  <Element />
-                </div>
-              )
+            {fields?.map(({ label, element: Element }) => (
+              <div key={label} className='flex flex-col gap-2'>
+                <Label>{label}</Label>
+                <Element />
+              </div>
+            ))}
+
+            {!fields && (
+              <span>
+                It seems like there's no editor for this type of component
+              </span>
             )}
           </div>
         </motion.div>
