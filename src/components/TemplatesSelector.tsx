d925b668a3c260907204036a0e2c8b5b5f13295b
Владислав <vladislav@MacBook-Air-Vladislav.local>
2025-01-23 23:57:57 +0300

    fix: templates selector borders

--- src/components/TemplatesSelector.tsx
+++ src/components/TemplatesSelector.tsx
@@ -23,9 +23,13 @@ export const TemplatesSelector = () => {
           key={templateName}
           onClick={() => selectTemplate(templateName as TemplateName)}
           className={cn(
-            'flex items-center justify-between p-2',
-            templateName === selectedTemplate &&
-              'rounded-xl border-2 dark:border-neutral-600'
+            'flex items-center justify-between rounded-xl border-2 p-2',
+            {
+              'border-neutral-200 dark:border-neutral-600':
+                templateName === selectedTemplate,
+              'border-transparent dark:border-transparent':
+                templateName !== selectedTemplate
+            }
           )}
         >
           <span className='font-arial-black'>
