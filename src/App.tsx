70f796bb791255cc8ccf51b588240ef143e7a1b6
Владислав <vladislav@MacBook-Air-Vladislav.local>
2025-01-27 19:31:44 +0300

    fix: minor ThemeContext changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,41 @@
 import { Layout } from './components/Layout'
 import { TemplateContextProvider } from './contexts/TemplateContext'
 import { TemplateEditorContextProvider } from './contexts/TemplateEditorContext'
-import { ThemeContext } from './contexts/ThemeContext'
 import { Account } from './pages/Account'
 import { Create } from './pages/Create'
 import { Pricing } from './pages/Pricing'
 import { Templates } from './pages/Templates'
-import { ThemeContextValues } from './types/contexts'
 import { Main } from '@/pages/Main'
-import { useContext } from 'react'
 import { Navigate, Outlet, Route, Routes } from 'react-router-dom'
 
 function App() {
-  const { theme } = useContext(ThemeContext) as ThemeContextValues
-
   return (
-    <div className={theme}>
-      <div className='dark:bg-neutral-900 dark:text-white'>
-        <Routes>
-          <Route
-            element={
-              <TemplateContextProvider>
-                <Outlet />
-              </TemplateContextProvider>
-            }
-          >
-            <Route
-              path='/create'
-              element={
-                <TemplateEditorContextProvider>
-                  <Create />
-                </TemplateEditorContextProvider>
-              }
-            />
-            <Route path='/' element={<Main />} />
-          </Route>
-          <Route path='/account' element={<Account />} />
-          <Route element={<Layout />}>
-            <Route path='/pricing' element={<Pricing />} />
-            <Route path='/templates' element={<Templates />} />
-          </Route>
-          <Route path='/dashboard' element={<Main />} />
-          <Route path='*' element={<Navigate to='/' />} />
-        </Routes>
-      </div>
-    </div>
+    <Routes>
+      <Route
+        element={
+          <TemplateContextProvider>
+            <Outlet />
+          </TemplateContextProvider>
+        }
+      >
+        <Route
+          path='/create'
+          element={
+            <TemplateEditorContextProvider>
+              <Create />
+            </TemplateEditorContextProvider>
+          }
+        />
+        <Route path='/' element={<Main />} />
+      </Route>
+      <Route path='/account' element={<Account />} />
+      <Route element={<Layout />}>
+        <Route path='/pricing' element={<Pricing />} />
+        <Route path='/templates' element={<Templates />} />
+      </Route>
+      <Route path='/dashboard' element={<Main />} />
+      <Route path='*' element={<Navigate to='/' />} />
+    </Routes>
   )
 }
 
--- src/contexts/ThemeContext.tsx
+++ src/contexts/ThemeContext.tsx
@@ -1,5 +1,5 @@
 import { ThemeContextValues } from '@/types/contexts'
-import { FC, ReactNode, createContext, useState } from 'react'
+import { FC, ReactNode, createContext, useEffect, useState } from 'react'
 
 export const ThemeContext = createContext<ThemeContextValues | null>(null)
 
@@ -17,8 +17,12 @@ export const ThemeContextProvider: FC<{ children: ReactNode }> = ({
   children
 }) => {
   const [theme, setTheme] = useState<'dark' | 'light'>(getTheme())
+  const root = window.document.documentElement
 
   const changeTheme = (theme: 'dark' | 'light') => {
+    root.classList.remove('light', 'dark')
+    root.classList.add(theme)
+
     setTheme(theme)
     localStorage.setItem('theme', theme)
   }
@@ -26,6 +30,11 @@ export const ThemeContextProvider: FC<{ children: ReactNode }> = ({
   const toggleTheme = () => {
     changeTheme(theme === 'light' ? 'dark' : 'light')
   }
+
+  useEffect(() => {
+    root.classList.remove('light', 'dark')
+    root.classList.add(theme)
+  }, [theme])
 
   return (
     <ThemeContext.Provider value={{ theme, changeTheme, toggleTheme }}>
